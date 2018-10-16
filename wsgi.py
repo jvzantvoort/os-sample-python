b16e625ec35bf53270b691fbd2ba1d42034699e1
John van Zantvoort <john@vanzantvoort.org>
2018-10-16 17:55:48 +0200

    minor update

--- wsgi.py
+++ wsgi.py
@@ -4,10 +4,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    retv = str()
-    for k, v in os.environ.items():
-        retv += "%s: %s\n" % (k, v)
-    return retv
+    return "Hello World from %s\n\n" % os.uname()[1]
 
 if __name__ == "__main__":
     application.run()
